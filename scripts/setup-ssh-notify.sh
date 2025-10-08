8ad079338492887f9bee5bb8a9da07dc10fd4e52
Abdolahi <94745812+Abdolahi34@users.noreply.github.com>
2025-10-08 07:11:41 +0330

    Update setup-ssh-notify.sh

--- scripts/setup-ssh-notify.sh
+++ scripts/setup-ssh-notify.sh
@@ -3,17 +3,17 @@
 # SSH Login Telegram Notifier
 # -----------------------------
 
-read -p "🔑 Enter the Server Name: " SERVER_NAME
-read -p "🔑 Enter the Telegram bot token: " BOT_TOKEN
-read -p "💬 Enter Telegram chat_id: " CHAT_ID
+read -p "[+] Enter the Server Name: " SERVER_NAME
+read -p "[+] Enter the Telegram bot token: " BOT_TOKEN
+read -p "[+] Enter Telegram chat_id: " CHAT_ID
 
 # Install curl if needed
-echo "📦 Install curl if needed..."
+echo "[*] Install curl if needed..."
 sudo apt update -y
 sudo apt install -y curl
 
 # Creating a notification sending script
-echo "📝 Create a notification script..."
+echo "[*] Create a notification script..."
 cat <<EOF | sudo tee /usr/local/bin/ssh_notify.sh >/dev/null
 #!/bin/bash
 if [ "$PAM_TYPE" != "open_session" ]; then
@@ -42,10 +42,10 @@ EOF
 sudo chmod +x /usr/local/bin/ssh_notify.sh
 
 # Add to PAM
-echo "⚙️ PAM configuration..."
+echo "[*] PAM configuration..."
 if ! grep -q "ssh_notify.sh" /etc/pam.d/sshd; then
     echo "session optional pam_exec.so seteuid /usr/local/bin/ssh_notify.sh" | sudo tee -a /etc/pam.d/sshd
 fi
 
-echo "✅ Settings done."
+echo "[*] Settings done."
 echo "Every time an SSH login is performed, a message is sent to Telegram 🚀"
